f4393e1498077d267523fa7ddeb5e56581ddb9f9
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2016-11-10 00:10:30 +0300

    recycler view items with drawable text

--- app/src/main/java/com/example/algol/MenuActivity.java
+++ app/src/main/java/com/example/algol/MenuActivity.java
@@ -2,6 +2,11 @@ package com.example.algol;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        String[] items = {"Bubble Sort", "Insertion Sort", "Quick Sort"};
+
+        RecyclerMenuAdapter adapter = new RecyclerMenuAdapter(items);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/com/example/algol/RecyclerMenuAdapter.java
+++ app/src/main/java/com/example/algol/RecyclerMenuAdapter.java
@@ -1,14 +1,14 @@
 package com.example.algol;
 
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.amulyakhare.textdrawable.TextDrawable;
 
 /**
  * Created by Сергей Пинкевич on 05.11.2016.
@@ -16,34 +16,39 @@ import java.util.ArrayList;
 
 public class RecyclerMenuAdapter extends RecyclerView.Adapter {
 
-    private ArrayList data;
+    private String[] items;
+
+    public RecyclerMenuAdapter(String[] items) {
+        this.items = items;
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView mIcon;
-        private TextView mTitle;
-        private ListView mList;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            mIcon = (ImageView) itemView.findViewById(R.id.recycler_icon);
-            mTitle = (TextView) itemView.findViewById(R.id.recycler_title);
-            mList = (ListView) itemView.findViewById(R.id.recycler_list);
+        private CardView mCardView;
+        public ViewHolder(CardView v) {
+            super(v);
+            mCardView = v;
+
         }
     }
 
     @Override
     public RecyclerMenuAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        return new ViewHolder(cv);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        CardView cardView = (CardView) holder.itemView;
+        ImageView imageView = (ImageView)cardView.findViewById(R.id.recycler_item_circle);
+        TextView textView = (TextView)cardView.findViewById(R.id.recycler_item_title);
+        textView.setText(items[position]);
+        TextDrawable drawable = TextDrawable.builder().buildRound(String.valueOf(textView.getText().charAt(0)), Color.RED);
+        imageView.setBackground(drawable);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.length;
     }
 }
