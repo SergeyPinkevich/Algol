28dc71ce26384cec9aa908f2e8fee78d10035a65
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2016-11-20 20:42:49 +0300

    JUnit testing progress

--- app/src/main/java/com/example/algol/algoritms/Sorting.java
+++ app/src/main/java/com/example/algol/algoritms/Sorting.java
@@ -6,10 +6,81 @@ package com.example.algol.algoritms;
 
 public class Sorting {
 
-    private int[] mArray;
+    /**
+     * Good way to randomize input array
+     * @param array
+     */
+    public static int[] shuffling(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int r = (int) (Math.random() * array.length);
+            int temp = array[r];
+            array[r] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
 
-    public Sorting(int[] array) {
-        this.mArray = array;
+    /**
+     * Move entries more than one position at a time by h-sorting the array
+     * It is modifying insertion sort
+     * Worst case O(n) = n^2
+     * Best case O(n) = n * log^2(n)
+     * Average case - depends from step
+     * @param array
+     */
+    public static int[] shellSort(int[] array) {
+        int h = 1;
+
+        while (h < array.length / 3)
+            h = 3 * h + 1;
+
+        while (h >= 1) {
+            for (int i = 0; i < array.length; i++)
+                for (int j = i; j >= h && array[j - 1] > array[j]; j -= h) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            h /= 3;
+        }
+        return array;
+    }
+
+    /**
+     * We create additional array for interim work
+     * O(n) = n * log(n)
+     * @param array
+     */
+    public static void mergeSort(int[] array) {
+        int aux[] = new int[array.length];
+        sort(array, aux, 0, array.length - 1);
+    }
+
+    public static void sort(int[] array, int[] aux, int lo, int hi) {
+        if (hi <= lo)
+            return;
+        int mid = lo + (hi - lo) / 2;
+        sort(array, aux, lo, mid);
+        sort(array, aux, mid + 1, hi);
+        merge(array, aux, lo, mid, hi);
+    }
+
+    public static void merge(int array[], int[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++)
+            aux[k] = array[k];
+
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid)
+                array[k] = aux[j++];
+            else if (j > hi)
+                array[k] = aux[i++];
+            else if (aux[j] < aux[i])
+                array[k] = aux[j++];
+            else
+                array[k] = aux[i++];
+        }
     }
 
     /**
@@ -18,11 +89,12 @@ public class Sorting {
      * O(n) = n^2
      * @param array
      */
-    public void bubbleSort(int[] array) {
+    public static int[] bubbleSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++)
             for (int j = 0; j < array.length - i - 1; j++)
                 if (array[j] > array[j + 1])
                     swap(j, j + 1, array);
+        return array;
     }
 
     /**
@@ -30,13 +102,14 @@ public class Sorting {
      * O(n) = n^2
      * @param array
      */
-    public void insertionSort(int[] array) {
+    public static int[] insertionSort(int[] array) {
         for (int i = 0; i < array.length; i++)
             for (int j = i; j > 0; j--)
                 if (array[j - 1] > array[j])
                     swap(j, j - 1, array);
                 else
                     break;
+        return array;
     }
 
     /**
@@ -46,7 +119,7 @@ public class Sorting {
      *
      * @param array
      */
-    public void selectionSort(int[] array) {
+    public static int[] selectionSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             int min = i;
             for (int j = i + 1; j < array.length; j++)
@@ -54,6 +127,7 @@ public class Sorting {
                     min = j;
             swap(min, i, array);
         }
+        return array;
     }
 
     /**
@@ -62,7 +136,7 @@ public class Sorting {
      * @param j
      * @param array
      */
-    public void swap(int i, int j, int[] array) {
+    public static void swap(int i, int j, int[] array) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
---
+++ app/src/test/java/com/example/algol/algoritms/SortingTest.java
@@ -0,0 +1,54 @@
+package com.example.algol.algoritms;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Suite;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Сергей Пинкевич on 20.11.2016.
+ */
+public class SortingTest {
+
+    private int[] array;
+    private int[] answer = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+    @Before
+    public void setUp() throws Exception {
+        array = Sorting.shuffling(answer);
+    }
+
+    @Test
+    public void shuffle() throws Exception {
+        array = Sorting.shuffling(answer);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(array[i] + " " + answer[i] + '\n');
+        }
+    }
+
+    @Test
+    public void shellSort() throws Exception {
+        assertArrayEquals(answer, Sorting.shellSort(array));
+    }
+
+    @Test
+    public void mergeSort() throws Exception {
+
+    }
+
+    @Test
+    public void bubbleSort() throws Exception {
+        assertArrayEquals(answer, Sorting.bubbleSort(array));
+    }
+
+    @Test
+    public void insertionSort() throws Exception {
+        assertArrayEquals(answer, Sorting.insertionSort(array));
+    }
+
+    @Test
+    public void selectionSort() throws Exception {
+        assertArrayEquals(answer, Sorting.selectionSort(array));
+    }
+}
