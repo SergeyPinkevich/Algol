3ee9a97d516bc3282a6fbea3ff51c1b23628688b
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2017-04-03 19:18:52 +0300

    first visualization

--- app/src/main/java/com/example/algol/InteractionFragment.java
+++ app/src/main/java/com/example/algol/InteractionFragment.java
@@ -6,12 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.example.algol.visualizer.SortingVisualizer;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InteractionFragment extends Fragment {
+
+    private FrameLayout mFrameLayout;
 
     public InteractionFragment() {
         // Required empty public constructor
@@ -26,7 +31,17 @@ public class InteractionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_interaction, container, false);
+        View view = inflater.inflate(R.layout.fragment_interaction, container, false);
+
+        mFrameLayout = (FrameLayout) view.findViewById(R.id.container_for_view);
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++)
+            array[i] = (int) (Math.random() * 10);
+        SortingVisualizer visualizer = new SortingVisualizer(getActivity().getApplicationContext());
+        visualizer.setData(array);
+        mFrameLayout.addView(visualizer);
+
+        return view;
     }
 
 }
